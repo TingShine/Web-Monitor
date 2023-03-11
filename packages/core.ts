4103f540af78b1a970ff6dc007e27143baf4367f
shinetingli <shinetingli@tencent.com>
2023-03-12 00:13:09 +0800

    feat: 新增初始化函数

--- packages/core.ts
+++ packages/core.ts
@@ -1,29 +1,43 @@
-import { IConfig } from "./types"
+import { CoreStatusEnum, IConfig } from "./types"
 import { getDefaultConfig } from "./config"
 import EventEmitter from "eventemitter3"
 
 /** 单例模式 */
 export class Core {
-  public static version = VERSION
+  public static version = '1.0.0'
   private config = getDefaultConfig()
   private static instance: Core
+  public status = CoreStatusEnum.UNINITIALIZED
 
   /** 生命周期 */
   public lifeCycle = new EventEmitter()
 
+  /** 无法通过new获取实例 */
   private constructor() {}
 
   /** 获取实例 */
   public static getInstance() {
     if (!Core.instance) {
-      return new Core()
+      Core.instance  = new Core()
     }
 
     return Core.instance
   }
 
+  /** 初始化 */
   public init(config : Partial<IConfig>) {
     this.setConfig(config)
+    this.status = CoreStatusEnum.INITIALIZED
+    this.lifeCycle.emit('initialized')
+  }
+  public static init(config : Partial<IConfig>) {
+    if (!Core.instance) {
+      const instance = new Core()
+      Core.instance = instance
+      instance.init(config)
+    } else {
+      Core.instance.init(config)
+    }
   }
 
   public setConfig(config: Partial<IConfig>) {
--- packages/types.ts
+++ packages/types.ts
@@ -5,6 +5,12 @@ export enum LogType {
   ERROR = 'error'
 }
 
+export enum CoreStatusEnum {
+  UNINITIALIZED = 'uninitialized',
+  INITIALIZED = 'initialized',
+  READY = 'ready',
+}
+
 export interface IConfig {
   /** 上报地址 */
   hostUrl: string;
